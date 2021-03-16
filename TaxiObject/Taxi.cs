862ff0e09c46ee491bd5cabf23c994d68470ee39
Rakandhiya Daanii <rakandhiya.daanii@gmail.com>
2021-03-16 11:58:36 +0700

    initial commit

---
+++ TaxiObject/Taxi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiObject
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            
+        }
+
+        public void PickUpPassenger()
+        {
+
+        }
+
+        public void DropOffPassenger()
+        {
+
+        }
+    }
+}
